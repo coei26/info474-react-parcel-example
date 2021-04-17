e80f493ec018bbbc307e1cb511704702944e9f78
coei26 <coei26@uw.edu>
2021-04-17 02:12:30 -0700

    edit App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { csv } from "d3-fetch";
 
 const App = () => {
+    csv("https://raw.githubusercontent.com/coei26/info474-react-parcel-example/main/execution_data.csv")
+        .then(data => console.log(data))
     return (
         <div>
             <h1>Exploratory Data Analysis, Assignment 2, INFO 474 SP 2021</h1>
